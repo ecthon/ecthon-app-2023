a25960fb02f2c979593d1775848757c0bee2e322
newecthon <ecthon@gmail.com>
2023-10-24 18:08:13 -0300

    feature/componet: Heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Header } from "@/components/Header";
+import { Heading } from "@/components/sections/Heading";
 
 export default function Home() {
   return (
     <div className="flex flex-col w-100% h-auto">
       <Header />
+      <Heading />
     </div>
   )
 }
