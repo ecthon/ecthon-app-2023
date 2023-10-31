bfb8d6ba0bbbef7bcc673f4ff3e672f1240161c4
newecthon <ecthon@gmail.com>
2023-10-30 22:06:20 -0300

    feature: components in UIGuide

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,7 @@ export function Header() {
     return(
         <header>
             <div className="flex justify-between w-3/5 h-20 items-center mx-auto">
-                <a href="">ecthon</a>
+                <a href="#" className="text-3xl text-gray-900 font-bold">ecthon<span className="text-emerald-500">.</span></a>
                 <nav className="flex">
                     <Hamburger rounded toggled={isOpen} toggle={setOpen}/>
                 </nav>
