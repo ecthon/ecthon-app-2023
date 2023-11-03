5095389e893ec1ba728c707c7c73720a6fe3448c
newecthon <ecthon@gmail.com>
2023-11-03 15:36:17 -0300

    style: hamburger button size

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -12,7 +12,7 @@ export function Header() {
             <div className="flex justify-between w-3/5 max-sm:w-4/5 h-20 max-sm:h-auto items-center mx-auto">
                 <a href="#" className="text-3xl max-sm:text-xl text-gray-900 font-bold">ecthon<span className="text-emerald-500">.</span></a>
                 <nav className="flex">
-                    <Hamburger rounded toggled={isOpen} toggle={setOpen} size={isMobile ? 18 : 32} />
+                    <Hamburger rounded toggled={isOpen} toggle={setOpen} size={isMobile ? 16 : 32} />
                 </nav>
             </div>
         </header>
