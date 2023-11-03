8461478535de84fa492d49a74d5a3a278952fb23
newecthon <ecthon@gmail.com>
2023-11-03 13:48:26 -0300

    style: responsive footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,15 +5,15 @@ export function Footer() {
     const currentYear = new Date().getFullYear()
 
     return (
-        <footer className="flex w-full h-20 m-auto">
-            <div className="flex justify-between items-center mx-auto w-3/5">
-                <div className="flex items-center gap-2">
-                    <p className="text-zinc-500">{currentYear}</p>
+        <footer className="flex w-full h-20 max-sm:h-auto m-auto">
+            <div className="flex max-sm:flex-col-reverse w-3/5 max-sm:w-4/5 max-sm:gap-2 max-sm:my-4 justify-between items-center mx-auto">
+                <div className="flex items-center gap-2 max-sm:gap-1">
+                    <p className="text-zinc-500 max-sm:text-sm">{currentYear}</p>
                     <Divider />
-                    <p className="text-zinc-500">
+                    <p className="text-zinc-500 max-sm:text-sm">
                         Code white Next.js 🖤 by
                     </p>
-                    <p className="text-zinc-500 hover:text-gray-900 font-medium cursor-pointer">@ecthon</p>
+                    <p className="text-zinc-500 max-sm:text-sm hover:text-gray-900 font-medium cursor-pointer">@ecthon</p>
                 </div>
                 <Socials />
             </div>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,18 @@
 'use client'
 import Hamburger from "hamburger-react";
 import { useState } from "react";
+import { useMediaQuery } from 'react-responsive';
 
 export function Header() {
     const [isOpen, setOpen] = useState(false)
+    // Detectar se a tela é menor que 640px (pode ajustar o valor conforme necessário)
+    const isMobile = useMediaQuery({ maxWidth: 640})
     return(
         <header>
-            <div className="flex justify-between w-3/5 max-sm:w-4/5 h-20 items-center mx-auto">
-                <a href="#" className="text-3xl max-sm:text-2xl text-gray-900 font-bold">ecthon<span className="text-emerald-500">.</span></a>
+            <div className="flex justify-between w-3/5 max-sm:w-4/5 h-20 max-sm:h-auto items-center mx-auto">
+                <a href="#" className="text-3xl max-sm:text-xl text-gray-900 font-bold">ecthon<span className="text-emerald-500">.</span></a>
                 <nav className="flex">
-                    <Hamburger rounded toggled={isOpen} toggle={setOpen} />
+                    <Hamburger rounded toggled={isOpen} toggle={setOpen} size={isMobile ? 18 : 32} />
                 </nav>
             </div>
         </header>
