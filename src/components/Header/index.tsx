1f7fec6e0361818d12b0052676797aef7a8b85b7
newecthon <ecthon@gmail.com>
2023-11-02 23:34:12 -0300

    resposive header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,8 +6,8 @@ export function Header() {
     const [isOpen, setOpen] = useState(false)
     return(
         <header>
-            <div className="flex justify-between w-3/5 h-20 items-center mx-auto">
-                <a href="#" className="text-3xl text-gray-900 font-bold">ecthon<span className="text-emerald-500">.</span></a>
+            <div className="flex justify-between w-3/5 sm:w-4/5 h-20 items-center mx-auto">
+                <a href="#" className="text-3xl sm:text-2xl sm:text-blue-600 text-gray-900 font-bold">ecthon<span className="text-emerald-500">.</span></a>
                 <nav className="flex">
                     <Hamburger rounded toggled={isOpen} toggle={setOpen}/>
                 </nav>
