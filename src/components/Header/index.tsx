d43c50b11efa2bbe83d505e3684c921de5c84313
newecthon <ecthon@gmail.com>
2023-11-04 01:57:17 -0300

    resize icon hamburger

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,16 +8,6 @@ export function Header() {
   const [isOpen, setOpen] = useState(false);
   const isMobile = useMediaQuery({ maxWidth: 640 });
 
-  const menuStyle = {
-    position: "fixed",
-    top: 0,
-    left: isOpen ? 0 : "-100%", // A posição inicial do menu fora da tela
-    width: isMobile ? "100%" : "300px",
-    height: "100%",
-    backgroundColor: "white",
-    transition: "left 0.3s", // Aplicando a transição na propriedade 'left'
-  };
-
   const toggleMenu = () => {
     setOpen(!isOpen);
   };
@@ -25,7 +15,7 @@ export function Header() {
   return (
     <div>
       <header>
-        <div className="flex justify-between w-3/5 max-sm:w-4/5 h-20 items-center mx-auto">
+        <div className="flex justify-between w-3/5 max-sm:w-4/5 h-20 max-sm:h-auto items-center mx-auto">
           <a href="#" className="text-3xl max-sm:text-xl text-gray-900 font-bold">
             ecthon<span className="text-emerald-500">.</span>
           </a>
