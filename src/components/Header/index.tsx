162b88d88d66ca999baa43ea2325b7be1a7a2974
newecthon <ecthon@gmail.com>
2023-11-05 22:07:48 -0300

    style: change header sm height

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -16,7 +16,7 @@ export function Header() {
   return (
     <div>
       <header>
-        <div className="flex justify-between w-3/5 max-sm:w-4/5 h-20 max-sm:h-16 items-center mx-auto">
+        <div className="flex justify-between w-3/5 max-sm:w-4/5 h-20 max-sm:h-12 items-center mx-auto">
           <a href="#" className="text-3xl max-sm:text-xl text-gray-900 font-bold">
             ecthon<span className="text-emerald-500">.</span>
           </a>
