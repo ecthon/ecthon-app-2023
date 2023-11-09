ef006fee97262fff9b44664bef9918b90d891a43
newecthon <ecthon@gmail.com>
2023-11-08 22:41:24 -0300

    fix: image in container

--- src/components/sections/Projects/index.tsx
+++ src/components/sections/Projects/index.tsx
@@ -5,7 +5,7 @@ export function Projects() {
         <section className="flex flex-col gap-y-8 w-3/5 max-sm:w-4/5 mx-auto">
             <div className="flex flex-wrap w-full h-auto gap-8">    
                 <div className="button border-effect flex w-96 max-sm:w-100% h-96 max-sm:h-100% bg-slate-400 rounded-2xl overflow-hidden">
-                    <Image width={700} height={700} src="/todo-capa-png.png" alt="Capa do projeto todo list" />
+                    <Image width={700} height={700} className="object-contain object-bottom" src="/todo-capa-png.png" alt="Capa do projeto todo list" />
                 </div>
                 <div className="flex flex-1 min-w-96 h-96 bg-slate-700 rounded-2xl"></div>
             </div>
